462276b8bb0f6eda647e8eb47ca3c7b4466bd0ee
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-14 17:20:24 +0000

    decision tree classify

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn import tree
+from sklearn.model_selection import train_test_split as tts
+
+wine = datasets.load_wine()
+
+features = wine.data
+labels = wine.target
+
+train_feats, test_feats, train_labels, test_labels = tts(features, labels, test_size=0.2)
+
+clf = tree.DecisionTreeRegressor()
+
+# train
+
+clf.fit(train_feats, train_labels)
+
+
+# predictions
+
+predictions = clf.predict(test_feats)
+
+print (predictions)
+
+score = 0
+for i in range(len(predictions)) :
+        if predictions[i] == test_labels[i]:
+                score +=1 
+print (score / len(predictions))
